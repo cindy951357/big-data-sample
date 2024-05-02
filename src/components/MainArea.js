c774cb6eb88f22c8941ab2412042818225929ac6
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2024-05-02 16:45:15 +0800

    rows-of-period

--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -1,9 +1,29 @@
 import { FilterCaseBar } from "./FilterCaseBar";
 
+const RowsOfPeriod = () => {
+    return (
+        <div className="rows-of-period w-full flex flex-col items-end pt-[20px] pr-[20px]">
+            <div className="text-[#52697A] flex items-center">
+                <div className="rounded-full w-[12px] h-[12px] bg-[#52697A] mr-[4px]"></div>
+                前期1346筆
+            </div>
+            <div className="text-[#5084E9] flex items-center">
+                <div className="rounded-full  w-[12px] h-[12px] bg-[#5084E9] mr-[4px]"></div>
+                當期1483筆
+            </div>
+        </div>
+    )
+}
+
 const MainArea = () => {
     return (
-        <div className="main-area">
-            <FilterCaseBar/>
+        <div className="main-area-bg bg-gray-100 h-full overflow-hidden">
+            <div className="main-area mt-[20px] ml-[20px] bg-white rounded-tl-lg h-full
+                pt-[20px]
+            ">
+                <FilterCaseBar/>
+                <RowsOfPeriod/>
+            </div>
         </div>
     )
 }
