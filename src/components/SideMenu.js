6e820ff4396a245852708bf46e1112e899c5fb9b
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2024-05-03 11:12:09 +0800

    WIP: API connecting

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,14 @@
 import "./SideMenu.scss";
+import IconChart from "../assets/images/icon-chart.png";
 const SideMenu = () => {
     return (
         <div className="side-menu-container w-[224px] h-full overflow-hidden">
-
+            <div className="menu-item-container w-full h-[56px] mt-[32px] bg-[#FDEDF1] flex items-center
+                px-[24px]
+            ">
+                <img src={IconChart} alt='chart'className="w-[26px] h-[22px] mr-[19px]"/>
+                <span className="menu-text text-[#E95076] text-sm font-bold">案件分析</span>
+            </div>
         </div>
     )
 }
